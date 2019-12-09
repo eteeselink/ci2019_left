f95cd8c5f3b8297c36a30a7aa3dfb7ca2778ed0b
Niels Rood <saxerniels@hotmail.com>
2019-12-09 14:54:37 +0100

    result display, also calling chart render function

--- src/poll.js
+++ src/poll.js
@@ -23,17 +23,18 @@ export class Poll {
             <button id="btn">Vote!</button>
         `;
 
-        this.element.querySelector("button").addEventListener("click", ev => {
+        this.element.querySelector("#btn").addEventListener("click", ev => {
             // always add `preventDefault` in an event handler. otherwise, the browser
             // will do some default action which usually means submitting the data to the server, 
             // which causes the entire page to reload.
             // since we have no server, we don't want that :-)
             ev.preventDefault();
 
-            const bestPizza = this.element.querySelector("input[name=pizza]:checked").value;
-            this.element.innerHTML = `<p>Indeed ${name}, Pizza ${bestPizza} is by far the best.</p><div id="pizza"></div>`;
+            const answer = this.element.querySelector("input[name=pizza]:checked").value;
+            this.element.innerHTML = `<p>The answer that was picked was ${answer}.</p><div id="pizza"></div>`;
             
-            makeAsciiArt(this.element.querySelector("#pizza"));
+            //makeAsciiArt(this.element.querySelector("#pizza"));
+            renderResultChart([value]);
         })
     }
 }
